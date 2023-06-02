201d5a8748f8ca4c3c284bb9663040a64f782ca3
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-02 15:55:43 +0530

    [Abhinav]-adding part time part

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ switch (n)
         }
 
         break;
+
+      //uc2
     case 2:
         int is_full_time = 1;
         int emp_rate_per_hour = 20;
@@ -40,6 +42,34 @@ switch (n)
         }
         empWage = empHrs * emp_rate_per_hour;
         Console.WriteLine("Emp Wage " + empWage);
+        break;
+
+        //uc3
+    case 3:
+        int is_part_time = 1;
+        int is_full_time1 = 2;
+        int emp_rate_per_hour_1 = 20;
+
+        int empHrs1 = 0;
+        int empWage1 = 0;
+        Random random2 = new Random();
+        int empCheck2 = random2.Next(0, 2);
+        Console.WriteLine(empCheck2);
+
+        if (empCheck2 == is_full_time1)
+        {
+            empHrs1 = 8;
+        }
+        else if(empCheck2 == is_part_time)
+        {
+            empHrs1 = 4;
+        }
+        else
+        {
+            empHrs1 = 0;
+        }
+        empWage1 = empHrs1 * emp_rate_per_hour_1;
+        Console.WriteLine("Emp Wage " + empWage1);
         break;
     default:
         Console.WriteLine("Wrong Input Entered");
